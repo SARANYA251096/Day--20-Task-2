adf6b75b07bb469ba773994c3935cfa37cc87fac
SARANYA251096 <110889324+SARANYA251096@users.noreply.github.com>
2022-09-20 10:48:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+async function foo() {
+
+    try {
+        let res = await fetch("https://hp-api.herokuapp.com/api/characters/students");
+        let res1 = await res.json();
+        for(let i=0;i<res1.length;i++){
+            console.log(res1);
+        }
+        let div = document.createElement("div");
+        div.classList.add("container-fluid", "d-flex:", "justify-content:center", "m-5", "p-5")
+        div.innerHTML = `<div class="card text-white bg-secondary mb-3" style="max-width: 400px; margin-left:400px;" >
+        <div class="card-header" style="text-align:center;">Harry potter characters</div>
+        <div class="card-body">
+        <p class="card-text">Name:    ${res1[0].name}</p>
+        <p class="card-text">Gender:    ${res1[0].gender}</p>
+        <p class="card-text">DOB:    ${res1[0].dateOfBirth}</p>
+        
+        <img src="${res1[0].image}" class="card-img-top">
+        </div>
+      </div>
+        </div>
+      </div>`
+        document.body.append(div);
+  
+    } 
+    catch (error) {
+        console.log(error);
+    }
+  
+  }
+  foo();
